4317d6c0b625cf3d209bf50e41724f22a613701f
mz106 <flashman123@fastmail.fm>
2021-10-07 15:00:01 +0100

    third function added

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const testFunction = (par1, par2) => {
 
 const secondFunction = (par) => {
     const squared = par ** 2;
-    console.log(`Your number ${par} is ${squared} when squared.`)
+    console.log(`Your number ${par} is ${squared} when squared.`);
+};
+
+const thirdFunction = (par1, par2) => {
+    const exponent = par1 ** par2;
+    console.log(`${par1} raised to the power ${par2} is ${exponent}`);
 };
 
 module.exports = {
