b8dc26610ea5bfc1186288e5f7f75a628d972143
mz106 <flashman123@fastmail.fm>
2021-10-07 12:25:27 +0100

    added squared function

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const testFunction = (par1, par2) => {
     console.log(`These numbers multiplied together come to ${num}.`);
 };
 
+const secondFunction = (par) => {
+    const squared = par ** 2;
+    console.log(`Your number ${par} is ${squared} when squared.`)
+};
+
 module.exports = testFunction;
